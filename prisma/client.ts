ffb103fc932ddb417cc8d266458abcdd0e39ce3a
Chris Abrams <mail@chrisabrams.com>
2024-11-29 11:01:03 -0600

    feature: support deno 2.1 explicity commonjs modules (#2)

--- prisma/client.ts
+++ prisma/client.ts
@@ -2,7 +2,7 @@ import { createRequire } from "node:module";
 import type { PrismaClient } from "./generated/client/index.d.ts";
 
 const require = createRequire(import.meta.url);
-const Prisma = require("./generated/client/index.js");
+const Prisma = require("./generated/client/index.cjs");
 export const prisma: PrismaClient = new Prisma.PrismaClient();
 
 export * from "./generated/client/index.d.ts";
